2287bf8a348469a7fc3c6ef04e9a321b1aa468c0
Zhenya <emakolov@bk.ru>
2026-08-09 08:07:06 +0300

    fixed select requests

--- select_requests_task2.sql
+++ select_requests_task2.sql
@@ -2,8 +2,7 @@ SELECT name, year FROM albums -- 1
 WHERE year = 2018;
 
 SELECT name, duration FROM tracks -- 2
-ORDER BY duration DESC
-LIMIT 1;
+WHERE duration = (SELECT max(duration) FROM tracks);
 
 SELECT name FROM tracks
 WHERE duration >= 210; -- 3
@@ -15,4 +14,4 @@ SELECT name FROM perfomers -- 5
 WHERE name NOT LIKE '% %';
 
 SELECT name FROM tracks -- 6
-WHERE name LIKE '%my%' OR name LIKE '%My%';
+WHERE name iLIKE '%my%' OR name iLIKE '%мой%';
